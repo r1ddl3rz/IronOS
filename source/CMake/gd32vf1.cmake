eb9b9ddefda517566254218d49a6d9dbff6b63f9
Ben V. Brown <Ralim@Ralimtek.com>
2022-03-31 22:49:41 +1100

    WiP Pinecil

--- source/CMake/gd32vf1.cmake
+++ source/CMake/gd32vf1.cmake
@@ -4,4 +4,4 @@ set(CORE_FLAGS "-march=rv32imac -mabi=ilp32 -mcmodel=medany -fsigned-char -fno-b
 add_definitions(${CORE_FLAGS})
 # link with linker file
 target_link_libraries(${elf_file} PUBLIC -T ${CMAKE_CURRENT_SOURCE_DIR}/linkers/gd32vf103.ld)
-target_link_libraries(${elf_file} PUBLIC -nostartfiles --specs=patch.specs)
+target_link_libraries(${elf_file} PUBLIC -nostartfiles --specs=${CMAKE_CURRENT_SOURCE_DIR}/patch.specs)
